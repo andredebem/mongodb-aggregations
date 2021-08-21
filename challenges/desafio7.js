4abe00b8ad6d2cf3c3e0ceb01909d242f95517b3
André De Bem <andrebemferraz@gmail.com>
2021-08-21 17:00:24 -0300

    Request VII

---
+++ challenges/desafio7.js
@@ -0,0 +1,34 @@
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        { cast: { $exists: true } },
+        { languages: { $all: ["English"] } },
+      ],
+    },
+  },
+  {
+    $unwind: "$cast",
+  },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB_sem_round: {
+        $avg: "$imdb.rating",
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      numeroFilmes: 1,
+      mediaIMDB: {
+        $round: [
+          "$mediaIMDB_sem_round",
+          1,
+        ],
+      },
+    },
+  },
+]);

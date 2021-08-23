04243f3d4d087d32aa319d57662d30f2e53cf522
André De Bem <andrebemferraz@gmail.com>
2021-08-23 16:34:24 -0300

    Request IX

---
+++ challenges/desafio9.js
@@ -0,0 +1,28 @@
+db.trips.aggregate([
+  {
+    $match: {
+      $and: [
+        { birthYear: { $exists: true } },
+        { birthYear: { $ne: "" } },
+      ],
+    },
+  },
+  {
+    $addFields: {
+      birthYearInt: {
+        $toInt: "$birthYear",
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: {
+        $max: "$birthYearInt",
+      },
+      menorAnoNascimento: {
+        $min: "$birthYearInt",
+      },
+    },
+  },
+]);
